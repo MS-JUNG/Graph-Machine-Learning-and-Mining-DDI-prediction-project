d5d52dc9139bbdd4a26174e1595b6ed3fe16cf54
Minsu Jung <95337623+MS-JUNG@users.noreply.github.com>
2024-05-31 14:12:19 +0900

    Update run_2_classifier.sh

--- run_2_classifier.sh
+++ run_2_classifier.sh
@@ -10,3 +10,9 @@ ORDER="GCN GAT GraphSAGE GIN"
 python train_fc_v1.py > results/result_fc_v1.out
 python train_fc_v2.py > results/result_fc_v2.out
 
+# 2. simple K-Way edge predictor
+python train_kway_v1.py > results/result_kway_v1.out
+for i in $ORDER
+do
+ python train_kway_v2.py $i > results/result_kway_v2_${i}.out
+done
